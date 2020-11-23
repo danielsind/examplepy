679ad5a45435873a3ed5b54ee4ffea2042076775
danielsind <danielsindet@gmail.com>
2020-11-23 11:43:34 +0300

    initial

---
+++ example.py
@@ -0,0 +1,11 @@
+import random
+import math
+
+radius = random.randint(0,10)
+pie = 3.14
+circumference = 2 * pie * radius
+print ("This radius is " + str(radius) + " therefore the circumfrence is " + str(circumference))
+if circumference > 20 :
+    print ("WAAAAAA Really big")
+else :
+        print("Ndogo bado")
